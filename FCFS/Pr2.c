9ae929d1d43062c2c18cd7ecffd2d69089b16fa6
Eduardo Pérez <69828130+tiero-x@users.noreply.github.com>
2020-09-15 16:36:57 -0500

    Guardado

--- FCFS/Pr2.c
+++ FCFS/Pr2.c
@@ -9,8 +9,8 @@ struct proceso{
   int rafagas_CPU[11];
   int rafagas_ES[10];
   int tiempo_llegada;
-  int tiempo_espera;
-  int tiempo_vuelta;
+  int waiting_time;
+  int turnaround_time;
   int tat;
 };
 
@@ -60,6 +60,7 @@ void generador_procesos(int ale){
 }
 
 void imprimir_procesos(int ale){
+  int sum_waiting_time = 0, sum_turnaround_time;
   for(int i=0;i<20;i++){
     printf("\n PROCESO # %d\n",i+1);
     printf("PID: %d\n",proc[i].pid);
@@ -73,6 +74,39 @@ void imprimir_procesos(int ale){
     printf("\n");
     printf("LLEGADA: %d\n",proc[i].tiempo_llegada);
     printf("\n");
+
+    proc[i].waiting_time = proc[i].turnaround_time = 0;
+}
+
+// calculate waiting time and turnaround time
+proc[0].turnaround_time = proc[0].burst_time;
+
+for(i=1; i<20; i++) {
+    proc[i].waiting_time = proc[i-1].waiting_time + proc[i-1].burst_time;
+    proc[i].turnaround_time = proc[i].waiting_time + proc[i].burst_time;
+}
+
+// calculate sum of waiting time and sum of turnaround time
+for(i=0; i<20; i++) {
+        sum_waiting_time += proc[i].waiting_time;
+        sum_turnaround_time += proc[i].turnaround_time;
+}
+
+// print table
+puts(""); // Empty line
+print_table(p, n);
+puts(""); // Empty Line
+printf("Total Waiting Time      : %-2d\n", sum_waiting_time);
+printf("Average Waiting Time    : %-2.2lf\n", (double)sum_waiting_time / (double) n);
+printf("Total Turnaround Time   : %-2d\n", sum_turnaround_time);
+printf("Average Turnaround Time : %-2.2lf\n", (double)sum_turnaround_time / (double) n);
+
+// print Gantt chart
+puts(""); // Empty line
+puts("          GANTT CHART          ");
+puts("          ***********          ");
+print_gantt_chart(p, 20);
+return 0;
   }
 }
 
@@ -84,37 +118,4 @@ int generar_aleatorio()
     aleatorio=rand()%10;
   }
   return aleatorio;
-}
-
-int tiempo()
-{
-  int total_wt=0, total_tat=0;
-
-    proc[0].tiempo_espera = 0;
-
-    for(int i=1;i<proc[i].pid;i++)
-    {
-      proc[i].tiempo_espera = proc[i-1].rafagas_CPU[i] + proc[i-1].tiempo_espera;
-    }
-
-    for (int i=0;i<proc[i].pid;i++)
-    {
-      proc[i].tat=proc[i].tiempo_espera + proc[i].rafagas_CPU[i];
-    }
-
-  printf("Procesos  Rafaga  Tiempo de espera  Tiempo de llegada\n");
-  for(int i=0;i<proc[i].pid;i++)
-  {
-    total_wt = total_wt + proc[i].tiempo_espera;
-    total_tat= total_tat + proc[i].tat;
-    printf("%d", (i+1));
-    printf("%d", proc[i].rafagas_CPU[i]);
-    printf("%d", proc[i].tiempo_espera);
-    printf("%d", proc[i].tat);
-  }
-  int i;
-  int s=(float)total_wt/(float)proc[i].pid;
-  int t=(float)total_tat/(float)proc[i].pid;
-  printf("Tiempo de espera = %d\n",s);
-  printf("Tiempo de vuelta = %d\n",t);
 }
